cb48af9a2c6bc38b00352a2a5ff3e944c5a9b73a
Koszta Dániel <daniel.koszta@gmail.com>
2020-12-28 15:55:22 +0100

    Initialize project (with Intellij IDEA)
    Excluded directory `.gradle` from commit.
    
    Change-Id: I999e035d0e2e5c5d155c956963e0d396186ec19d

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "dev.kosztadani"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
